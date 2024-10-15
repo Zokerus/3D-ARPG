5dae1b67e7e25f30ad8a8e676ef384435f8a5378
Zokerus <Zokerus@users.noreply.github.com>
2024-10-15 07:30:06 +0200

    add primitive player movement
    add basic player character script and movement

--- Character/Camera/camera_controller.gd
+++ Character/Camera/camera_controller.gd
@@ -1,10 +1,15 @@
 extends Node3D
 class_name CameraController
 
-@export var character: CharacterBody3D
+@export var character: PlayerCharacter
+
+var h_Sensetivity: float = 100
+var v_Sensetivity: float = 100
+var Invert_YAxis: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	position = character.position
 
 
