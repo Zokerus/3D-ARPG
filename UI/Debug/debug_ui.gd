9a08eccccdcf43c80237c6481d694da5608150d4
Zokerus <Zokerus@users.noreply.github.com>
2024-10-17 13:41:34 +0200

    exchanged player character model in main scene
    create b asic animation tree with idle animation

--- UI/Debug/debug_ui.gd
+++ UI/Debug/debug_ui.gd
@@ -1,7 +1,7 @@
 extends Control
 class_name DebugUI
 
-@export var player: SimplePlayerCharacter
+@export var player: PlayerCharacter
 
 @onready var label: Label = $Label
 
@@ -10,4 +10,4 @@ func _process(delta: float) -> void:
 	var player_position: Vector3 = player.global_position
 	label.text = "Player Position: (%.2f, %.2f, %.2f)\n" % [player_position.x, player_position.y, player_position.z]
 	label.text += "Player Direction: %.2f\n" % [player.rotation_degrees.y]
-	label.text += "Movement Direction: (%.2f, %.2f)\n" % [player.movementDirection.x, player.movementDirection.z]
+	#label.text += "Movement Direction: (%.2f, %.2f)\n" % [player.movementDirection.x, player.movementDirection.z]
