95371317e85834f760c7540b67fbd911640cc477
Zokerus <Zokerus@users.noreply.github.com>
2024-10-21 13:43:42 +0200

    add character animation and add root motion

--- Character/Player/player.gd
+++ Character/Player/player.gd
@@ -3,9 +3,17 @@ class_name PlayerCharacter
 
 @export var mainCamera: CameraController
 
+@onready var animationTree: AnimationTree = $AnimationTree
+
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const CHARACTER_ROTATION_RATE = 4*PI;
 
+var movementDirection: Vector3 = Vector3.ZERO
+
+func _ready() -> void:
+	if !mainCamera:
+		get_tree().quit()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -16,15 +24,24 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	CalculateMovement(delta)
+	var currentRotation: Quaternion = transform.basis.get_rotation_quaternion()
+	var root_position: Vector3 = animationTree.get_root_motion_position()
+	var root_velocity: Vector3 = (currentRotation.normalized() * root_position) / delta
+	
+	velocity.x = root_velocity.x
+	velocity.y -= root_velocity.y
+	velocity.z = root_velocity.z
+	
 	move_and_slide()
+	mainCamera.position = self.position
+
+func CalculateMovement(delta: float) -> void:
+	var input_dir: Vector2 = Input.get_vector("Left", "Right", "Forward", "Backward")
+	movementDirection = (mainCamera.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	animationTree.set("parameters/Running/blend_position", input_dir)
+	if movementDirection != Vector3.ZERO:
+		#var rotation_Angle: float = -transform.basis.z.signed_angle_to(movementDirection, Vector3.UP)
+		#self.rotation.y = lerp_angle(self.rotation.y, atan2(-movementDirection.x, -movementDirection.z), CHARACTER_ROTATION_RATE * delta)
+		pass
+		
